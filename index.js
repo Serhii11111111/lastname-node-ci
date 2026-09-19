f2185c2ff5c4b63c771f3db8fbf1eae210fc7d94
Serhii11111111 <matalo2131@gmail.com>
2026-09-19 16:50:23 +0300

    Add EC2 CI/CD deployment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+const http = require('http');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  res.end('<h1>Hello from AWS EC2!</h1><p>CI/CD deployment works.</p>');
+});
+
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on port ${port}`);
+});
